08960ced63dde7b12e3ab3716740bb9cf5aba953
AnaYandun2105 <anyandun1995@gmail.com>
2025-06-10 09:43:54 -0500

    iconos

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 35, 196, 207);
+
+const List<Color> colorlist = [
+  Color.fromARGB(255, 58, 113, 208),
+  Color.fromARGB(255, 198, 140, 210),
+];
+
+class AppTheme {
+  int selector = 0;
+
+  AppTheme((required this.selector))
+  : assert (selector >= 0, "El selector debe ser mayor o igual a 0");
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: 
+  )
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:inspira_app/config/theme/app_theme.dart';
+//getx
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,30 +10,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application. It is stateless, meaning that
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Inspira_app',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: const Color.fromARGB(255, 142, 168, 190)),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Inspira App')),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Bienvenido Inspira!'),
+              ElevatedButton(
+                onPressed: () {
+                  Get.print(
+                    'Hola Mundo',
+                  ); // Don't invoke print in production code.
+                },
+                child: const Text('Hola Mundo'),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
